104d69527e264172f9e3e6dec6a68a0b3ac17527
LauRobles <robleslaura663l@email.com>
2022-09-29 13:37:35 -0500

    Inserta Maestros

--- PaginasVista/maestros_datos_per.php
+++ PaginasVista/maestros_datos_per.php
@@ -15,26 +15,24 @@
                     <img src="/logo_pagina/logo-tecnm-2018_orig.png" alt="" width="30%" >
                 </div>
             </div>
-            <form class="cajas_de_texto_maestros"  action="/ModificacionesBD/InsertaMaes.php" method ="POST">
-                <input class="caja_texto" type="text" placeholder="Clave" id="clave"><br>
-                <input class="caja_texto" type="text" placeholder="Nombre" id="nombre"><br>
-                <input class="caja_texto" type="text" placeholder="Apellido paterno" id="apePat"><br>
-                <input class="caja_texto" type="text" placeholder="Apellido materno" id="apeMat"><br>
-                <input class="caja_texto" type="text" placeholder="Calle y número" id="calle"><br>
-                <input class="caja_texto" type="text" placeholder="Colonia" id="colonia"><br>
-                <input class="caja_texto" type="text" placeholder="Municipio" id="munnicipio"><br>
-                <input class="caja_texto" type="text" placeholder="Estado" id="estado"><br>
-                <input class="caja_texto" type="int" placeholder="Código postal" id="cp"><br>
-                <input class="caja_texto" type="int" placeholder="No.Teléfono" id="telefono"><br>
-                <input class="caja_texto" type="text" placeholder="RFC" id="rfc"><br>
-                <input class="caja_texto" type="text" placeholder="Titulo" id="titulo"><br>
-                <input class="caja_texto" type="email" placeholder="Correo" id="correo">
+            <form  method="POST" action="/ModificacionesBD/InsertaMaes.php" class="cajas_de_texto_maestros" >
+                <input class="caja_texto" type="text" placeholder="Clave" name="clave" id="clave"><br>
+                <input class="caja_texto" type="text" placeholder="Nombre" name="nombre" id="nombre"><br>
+                <input class="caja_texto" type="text" placeholder="Apellido paterno" name="apePat" id="apePat"><br>
+                <input class="caja_texto" type="text" placeholder="Apellido materno" name="apeMat"  id="apeMat"><br>
+                <input class="caja_texto" type="text" placeholder="Calle y número" name="calle" id="calle"><br>
+                <input class="caja_texto" type="text" placeholder="Colonia"  name="colonia" id="colonia"><br>
+                <input class="caja_texto" type="text" placeholder="Municipio" name="municipio" id="municipio"><br>
+                <input class="caja_texto" type="text" placeholder="Estado" name="estado" id="estado"><br>
+                <input class="caja_texto" type="int" placeholder="Código postal" name="cp" id="cp"><br>
+                <input class="caja_texto" type="int" placeholder="No.Teléfono" name="telefono" id="telefono"><br>
+                <input class="caja_texto" type="text" placeholder="RFC" name="rfc" id="rfc"><br>
+                <input class="caja_texto" type="text" placeholder="Titulo" name="titulo" id="titulo"><br>
+                <input class="caja_texto" type="email" placeholder="Correo" name="correo" id="correo"><br><br>
+                <input type="submit" value="Guardar" name="guarda_sec" class="btn" ><br>
+                <input type="submit" value="Cancelar" name="cancela_sec" class="btn">
             </form>
         </div>
-    </div>
-    <div class="botones">
-        <a id="guardar_boton" href="/ModificacionesBD/InsertaMaes.php">Guardar</a>
-        <a id="boton_cancelar" href="jefe_Control.php">Cancelar</a>
     </div>
 
     
