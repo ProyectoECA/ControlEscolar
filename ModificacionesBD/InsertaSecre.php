f2f3eb8af458c1f7f12771fd5494aa1ab489a969
FanySaLu <fanylulo@hotmail.com>
2022-09-28 19:12:57 -0500

    ..

--- ModificacionesBD/InsertaSecre.php
+++ ModificacionesBD/InsertaSecre.php
@@ -3,8 +3,26 @@ include_once '../CRUD/CRUD_bd_SQLServer.php';
 
 class Insertar_Secretaria extends CRUD_SQL_SERVER{
     function insertando(){
-        $crud = new CRUD_SQL_SERVER();
-        $crud->conexionBD();
+        $this->conexionBD();
+
+        $no_empleado=$_POST["numeroEmple"];
+        $nombre=$_POST["nombre"];
+        $ape_Pat=$_POST["apellidoP"];
+        $ape_Mat=$_POST["appelidoM"];
+        $calle=$_POST["calle"];
+        $colonia=$_POST["colonia"];
+        $municipio=$_POST["municipio"];
+        $estado=$_POST["estado"];
+        $codPos=$_POST["cp"];
+        $telefono=$_POST["tel"];
+        $email=$_POST["correo"];
+
+        $query="INSERT INTO [Secretarias],(IdSec,Nombre,ApePaterno,ApeMaterno,Telefono,Correo,Calle,Colonia) 
+        VALUES(?,?,?,?,?,?,?,?)";
+        $parametros=array($no_empleado,$nombre,$ape_Pat,$ape_Mat,$telefono,$email,$calle,$colonia);
+
+        #INSERTA EN TABLA SECRETARIAS
+        $this->Insertar_Eliminar_Actualizar($query,$parametros);
     }
 }
 ?>
--- PaginasVista/secretarias.php
+++ PaginasVista/secretarias.php
@@ -15,18 +15,18 @@
                     <img src="/logo_pagina/logo-tecnm-2018_orig.png" alt="" width="30%" >
                 </div>
             </div>
-            <from class="cajas_de_texto_secretaria">
-                <input class="caja_texto" type="text" placeholder="No.Empleado"><br>
-                <input class="caja_texto" type="text" placeholder="Nombre"><br>
-                <input class="caja_texto" type="text" placeholder="Apellido paterno"><br>
-                <input class="caja_texto" type="text" placeholder="Apellido materno"><br>
-                <input class="caja_texto" type="text" placeholder="Calle y numero"><br>
-                <input class="caja_texto" type="text" placeholder="Colonia"><br>
-                <input class="caja_texto" type="text" placeholder="Municipio"><br>
-                <input class="caja_texto" type="text" placeholder="Estado"><br>
-                <input class="caja_texto" type="int" placeholder="Codigo postal"><br>
-                <input class="caja_texto" type="int" placeholder="No.Telefono"><br>
-                <input class="caja_texto" type="email" placeholder="Correo">
+            <from class="cajas_de_texto_secretaria" action="/ModificacionesBD/InsertaSecre.php">
+                <input class="caja_texto" type="text" placeholder="No.Empleado" id="numeroEmple"><br>
+                <input class="caja_texto" type="text" placeholder="Nombre" id="nombre"><br>
+                <input class="caja_texto" type="text" placeholder="Apellido paterno" id="apellidoP"><br>
+                <input class="caja_texto" type="text" placeholder="Apellido materno"id="apellidoM"><br>
+                <input class="caja_texto" type="text" placeholder="Calle y numero" id="calle"><br>
+                <input class="caja_texto" type="text" placeholder="Colonia" id="colonia"><br>
+                <input class="caja_texto" type="text" placeholder="Municipio" id="municipio"><br>
+                <input class="caja_texto" type="text" placeholder="Estado" id="estado"><br>
+                <input class="caja_texto" type="int" placeholder="Codigo postal" id="cp"><br>
+                <input class="caja_texto" type="int" placeholder="No.Telefono" id="tel"><br>
+                <input class="caja_texto" type="email" placeholder="Correo" id="correo">
             </from>
         </div>
     </div>
