878815a77690d953cf55eb4f91cb62f5e7d674d0
Aldo6890 <aldocastillos433@gmail.com>
2022-10-01 15:05:40 -0500

    Me aparecieron estos cambios

--- Paginas Diseño/login.php
+++ Paginas Diseño/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilos_login.css">
+    <link rel="stylesheet" href="/css/estilos_login.css">
     <title>Login</title>
 </head>
 <body>
@@ -18,7 +18,7 @@
             <form class="inputs-container">
                 <input class="input" type="text" placeholder="Usuario">
                 <input class="input" type="password" placeholder="Contraseña">
-                <button class="btn"><a href="jefe_Control.html">Comprobar</button></a> 
+                <button class="btn"><a href="jefe_Control.php.html">Comprobar</button></a> 
                 <button class="btn"><a  href="">Recuperar</button></a>
             </form>
         </div>
