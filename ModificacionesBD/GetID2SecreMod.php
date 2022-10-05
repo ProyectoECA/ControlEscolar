94c4263a8f10fb66794ffbade4187f475e70da09
FanySaLu <fanylulo@hotmail.com>
2022-10-05 13:25:33 -0500

    modificasecre v1.4

--- ModificacionesBD/GetID2SecreMod.php
+++ ModificacionesBD/GetID2SecreMod.php
@@ -37,7 +37,7 @@ class Saca_ID{
         $query="SELECT IdSec FROM [Secretarias] where IdSec=?";
         $parametros=array($id);
         $stmt = sqlsrv_query($conexion, $query, $parametros);
-        $datos=sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC);?>
+        $datos=sqlsrv_fetch_array($stmt);?>
 
         <?php
         if(empty($datos)){?>
@@ -55,10 +55,20 @@ class Saca_ID{
                     }
                     else{
                         location.href='../PaginasVista/modificar_secretarias.html';
-                    }
-                    window.history.back('../PaginasVista/jefe_Control.html');})
+                    }})
                 </script>
         <?php
+        $id='';
+        $nom='';
+        $ap='';
+        $am='';
+        $cal='';
+        $col='';
+        $muni='';
+        $est='';
+        $codp='';
+        $telef='';
+        $cor='';
         }
         else{
             $query="SELECT Secretarias.IdSec,Nombre,ApePaterno,ApeMaterno,Calle,Colonia,Lugar.CP,Municipio,Estado,Telefono,Correo
@@ -66,20 +76,33 @@ class Saca_ID{
             where (Secretarias.IdSec=? and Secretarias.IdSec=LugSecretarias.IdSec and LugSecretarias.CP=Lugar.CP)";
             $parametros=array($id);
             $stmt2 = sqlsrv_query($conexion, $query, $parametros);
-            while($row=sqlsrv_fetch_array($stmt2,SQLSRV_FETCH_ASSOC)){?>
-                <form method="POST" action="/ModificacionesBD/ModificaSecre.php">
+            $row=sqlsrv_fetch_array($stmt2);
+                $id=$row['IdSec'];
+                $nom=$row['Nombre'];
+                $ap=$row['ApePaterno'];
+                $am=$row['ApeMaterno'];
+                $cal=$row['Calle'];
+                $col=$row['Colonia'];
+                $muni=$row['Municipio'];
+                $est=$row['Estado'];
+                $codp=$row['CP'];
+                $telef=$row['Telefono'];
+                $cor=$row['Correo']; 
+
+        }?>
+        <form method="POST" action="/ModificacionesBD/ModificaSecre.php">
                 <div class="contenedor-datos" style="float: center;">
-                <input class="conteDatos" type="text" placeholder="No. Empleado" name="clave2" value="<?php echo $row['IdSec'];?>" readonly>
-                <input class="conteDatos" type="text" placeholder="Nombre" name="nombre" value="<?php echo $row['Nombre'];?>">
-                <input class="conteDatos" type="text" placeholder="Ap. paterno" name="apellidoP" value="<?php echo $row['ApePaterno'];?>">
-                <input class="conteDatos2" type="text" placeholder="Ap. materno" name="apellidoM" value="<?php echo $row['ApeMaterno'];?>">
-                <input class="conteDatos2" type="text" placeholder="Calle y número" name="calle" value="<?php echo $row['Calle'];?>">
-                <input class="conteDatos2" type="text" placeholder="Colonia" name="colonia" value="<?php echo $row['Colonia'];?>">
-                <input class="conteDatos3" type="text" placeholder="Municipio" name="municipio" value="<?php echo $row['Municipio'];?>"> 
-                <input class="conteDatos3" type="text" placeholder="Estado" name="estado" value="<?php echo $row['Estado'];?>">
-                <input class="conteDatos3" type="text" placeholder="Código postal" name="cp" value="<?php echo $row['CP'];?>">
-                <input class="conteDatos4" type="text" placeholder="Teléfono" name="tel" value="<?php echo $row['Telefono'];?>">
-                <input class="conteDatos4" type="text" placeholder="Correo" name="correo" value="<?php echo $row['Correo'];?>">
+                <input class="conteDatos" type="text" placeholder="No. Empleado" name="clave2" value="<?php echo $id; ?>" readonly>
+                <input class="conteDatos" type="text" placeholder="Nombre" name="nombre" value="<?php echo $nom;?>">
+                <input class="conteDatos" type="text" placeholder="Ap. paterno" name="apellidoP" value="<?php echo $ap;?>">
+                <input class="conteDatos2" type="text" placeholder="Ap. materno" name="apellidoM" value="<?php echo $am;?>">
+                <input class="conteDatos2" type="text" placeholder="Calle y número" name="calle" value="<?php echo$cal;?>">
+                <input class="conteDatos2" type="text" placeholder="Colonia" name="colonia" value="<?php echo $col;?>">
+                <input class="conteDatos3" type="text" placeholder="Municipio" name="municipio" value="<?php echo $muni;?>"> 
+                <input class="conteDatos3" type="text" placeholder="Estado" name="estado" value="<?php echo $est;?>">
+                <input class="conteDatos3" type="text" placeholder="Código postal" name="cp" value="<?php echo $codp;?>">
+                <input class="conteDatos4" type="text" placeholder="Teléfono" name="tel" value="<?php echo $telef;?>">
+                <input class="conteDatos4" type="text" placeholder="Correo" name="correo" value="<?php echo $cor;?>">
                 </div>
                 <div class="contenedor-botones" style="float: center;">
                 <input class="botones" type="submit" name="modifica" value="EDITAR" onclick="location.href='/ModificacionesBD/ModificaSecre.php'">
@@ -89,10 +112,7 @@ class Saca_ID{
             </div>
             </body>
             </html>
-            <?php
-            }
-
-        }
+        <?php
     }
 }
 $id=new Saca_ID;
