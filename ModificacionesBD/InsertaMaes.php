09faf3998eb188746b4f1ced624bb81e5ccb8d86
LauRobles <robleslaura663l@email.com>
2022-10-06 19:49:45 -0500

    Correccion de ID Secretarias y Maestros

--- ModificacionesBD/InsertaMaes.php
+++ ModificacionesBD/InsertaMaes.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once '../CRUD/Usuarios_password.php';
+include_once "../CRUD/CRUD_bd_SQLServer.php";
 
 define("ServerName1", 'localhost');
 define("Database1", "ConEscolarNoc");
@@ -11,6 +12,8 @@ define("CharacterSet1", 'UTF-8');
 class Insertar_Maestros {
 
     function insertando(){
+        $cone=new CRUD_SQL_SERVER();
+        $cone->conexionBD();
 
         $conexion_pass = new User_password;
         $conexion_pass->conexionBD();
@@ -32,61 +35,74 @@ class Insertar_Maestros {
 
         $in= new Insertar_Maestros;
         if(isset($_POST['guarda_sec'])){
-        #INSERTA EN TABLA MAESTROS
-        try{
-            $connectionInfo = array("Database"=>Database1 , "UID"=>UID1, "PWD"=>PWD1, "CharacterSet"=>CharacterSet1);
-            $conexion=sqlsrv_connect(ServerName1, $connectionInfo);
 
-            $query= "INSERT INTO [Maestros] (ClaveMa,Nombre,ApePaterno,ApeMaterno,RFC,Titulo,Telefono,Correo,Calle,Colonia) 
-            VALUES (?,?,?,?,?,?,?,?,?,?)";
-            $parametros=array($clave,$nombre,$apePaterno,$apeMaterno,$rfc,$titulo,$telefono,$correo,$calle,$colonia);
-            $stmt= sqlsrv_query($conexion,$query, $parametros);
+            #COMPRUEBA QUE EL ID NO ESTE REGISTRADO
+            $query="SELECT * FROM [Maestros] where ClaveMa=?";
+            $parametros=array($clave);
+            $res=$cone->Buscar($query,$parametros);
+            $cone->CerrarConexion();
 
-            $query="SELECT * FROM [Lugar] where cp=?";
-            $parametros=array($cp);
-            $stmt= sqlsrv_query($conexion,$query, $parametros);
-            $arreResul= sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-            
-            #SI EL CP NO ESTA REGISTRADO AÚN LO AÑADE
-            if(empty($arreResul)){
-                $query= "INSERT INTO [Lugar] (CP, Municipio, Estado) VALUES (?,?,?)";
-                $parametros=array($cp, $municipio, $estado);
-                $stmt= sqlsrv_query($conexion,$query, $parametros);
-
-                $query= "INSERT INTO [LugMaestros] (ClaveMa,CP) VALUES (?,?)";
-                $parametros=array($clave,$cp);
-                $stmt= sqlsrv_query($conexion,$query, $parametros);
-
-                #Llamada a Alerta de registrado
-                $ban=true;
-                $in->alerts($ban);
-                
-                #Agrega contraseña en hash
-                $conexion_pass->InsertarUsuarioMaestro($clave, $clave);
-                $conexion_pass->CerrarConexion();
-
+            if($res[0][0]="ClaveMa")
+            {
+                echo "YA SE ENCUENTRA REGISTRADA ESA CLAVE";
             }
-            #SI EL CP YA ESTA REGISTRADO
             else{
-                $query= "INSERT INTO [LugMaestros] (ClaveMa,CP) VALUES (?,?)";
-                $parametros=array($clave,$cp);
-                $stmt= sqlsrv_query($conexion,$query, $parametros);
+                #INSERTA EN TABLA MAESTROS
+                try{
+                    $connectionInfo = array("Database"=>Database1 , "UID"=>UID1, "PWD"=>PWD1, "CharacterSet"=>CharacterSet1);
+                    $conexion=sqlsrv_connect(ServerName1, $connectionInfo);
 
-                #Llamada a Alerta de registrado
-                $ban=true;
-                $in->alerts($ban);
-                
-                #Agrega contraseña en hash
-                $conexion_pass->InsertarUsuarioMaestro($clave, $clave);
-                $conexion_pass->CerrarConexion();
-                
+                    $query= "INSERT INTO [Maestros] (ClaveMa,Nombre,ApePaterno,ApeMaterno,RFC,Titulo,Telefono,Correo,Calle,Colonia) 
+                    VALUES (?,?,?,?,?,?,?,?,?,?)";
+                    $parametros=array($clave,$nombre,$apePaterno,$apeMaterno,$rfc,$titulo,$telefono,$correo,$calle,$colonia);
+                    $stmt= sqlsrv_query($conexion,$query, $parametros);
+
+                    $query="SELECT * FROM [Lugar] where cp=?";
+                    $parametros=array($cp);
+                    $stmt= sqlsrv_query($conexion,$query, $parametros);
+                    $arreResul= sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+                    
+                    #SI EL CP NO ESTA REGISTRADO AÚN LO AÑADE
+                    if(empty($arreResul)){
+                        $query= "INSERT INTO [Lugar] (CP, Municipio, Estado) VALUES (?,?,?)";
+                        $parametros=array($cp, $municipio, $estado);
+                        $stmt= sqlsrv_query($conexion,$query, $parametros);
+
+                        $query= "INSERT INTO [LugMaestros] (ClaveMa,CP) VALUES (?,?)";
+                        $parametros=array($clave,$cp);
+                        $stmt= sqlsrv_query($conexion,$query, $parametros);
+
+                        #Llamada a Alerta de registrado
+                        $ban=true;
+                        $in->alerts($ban);
+                        
+                        #Agrega contraseña en hash
+                        $conexion_pass->InsertarUsuarioMaestro($clave, $clave);
+                        $conexion_pass->CerrarConexion();
+
+                    }
+                    #SI EL CP YA ESTA REGISTRADO
+                    else{
+                        $query= "INSERT INTO [LugMaestros] (ClaveMa,CP) VALUES (?,?)";
+                        $parametros=array($clave,$cp);
+                        $stmt= sqlsrv_query($conexion,$query, $parametros);
+
+                        #Llamada a Alerta de registrado
+                        $ban=true;
+                        $in->alerts($ban);
+                        
+                        #Agrega contraseña en hash
+                        $conexion_pass->InsertarUsuarioMaestro($clave, $clave);
+                        $conexion_pass->CerrarConexion();
+                        
+                    }
+                    sqlsrv_close($conexion);
+                }
+                catch(Exception $e){
+                    $ban=false;
+                }
             }
-            sqlsrv_close($conexion);
         }
-        catch(Exception $e){
-            $ban=false;
-        }
-    }
     else if(isset($_POST['cancela_sec'])){
         try{include_once "../PaginasVista/jefe_Control.html";
         }
@@ -95,7 +111,7 @@ class Insertar_Maestros {
         }
   
     }
-    }
+}
     function alerts($ban){
         #Alertas (necesitan html a fuerzas)
         ?>
