e4b8abe348f2b2e4364717add4f0e9f0620a4061
llamasdelacruz <tetillamas@gmail.com>
2022-10-27 17:42:09 -0500

    Que se limpien los campos despues de registrar una carrera

--- ManejoAlertas/alertas_registrar_carreras.js
+++ ManejoAlertas/alertas_registrar_carreras.js
@@ -1,3 +1,6 @@
+const clave_input = document.getElementById("clave");
+const nombre_input = document.getElementById("nombre");
+const semestres_select = document.getElementById("semestres");
 
 const form = document.getElementById("formulario");
 
@@ -13,8 +16,18 @@ xhr.onload = function () {
     if (xhr.status === 200) {
         // si la respuesta fue un JSON y no hubo ningun error
         var response = JSON.parse(xhr.responseText);
+        limpiar();
         alert(response.mensaje);
+
     }
 }
 xhr.send(data);
 });
+
+
+function limpiar() {
+    clave_input.value = "";
+    nombre_input.value = "";
+    semestres_select.value = "1";
+    
+}
