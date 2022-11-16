29a1d7c89f4f3c814667309f02ac2617b96e8974
llamasdelacruz <tetillamas@gmail.com>
2022-11-15 20:08:37 -0600

    Funcionalidad de la pagina principal_maestros.html

---
+++ php/principal_maestros.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+require_once("../CRUD/CRUD_bd_SQLServer.php");
+$conexion = new CRUD_SQL_SERVER();
+$conexion->conexionBD();
+
+$nombre = "";
+$materias = [];
+
+if(isset($_SESSION['user'])){
+
+    $nombre = $_SESSION['user'][2];
+    $user = $_SESSION['user'][0];
+
+
+    $sql = "SELECT AsigMaes.ClaveMat, Materias.Nombre FROM Materias,AsigMaes WHERE AsigMaes.Maestro = ? AND AsigMaes.ClaveMat = Materias.ClaveMat";
+    $parametros = array($user);
+    $materias = $conexion->Buscar($sql,$parametros);
+
+
+}
+$conexion->CerrarConexion();
+
+$data = ["nombre"=>$nombre,"materias"=>$materias];
+header("Content-Type: application/json");
+echo json_encode($data);
+
+?>
