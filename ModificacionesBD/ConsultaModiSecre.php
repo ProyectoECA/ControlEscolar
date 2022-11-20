fe887b588abb164358b97e071d02b372cfb6cbc7
ivan7351 <carlosivanmunoz3@gmail.com>
2022-11-19 20:03:16 -0600

    modificar secretaria

--- ModificacionesBD/ConsultaModiSecre.php
+++ ModificacionesBD/ConsultaModiSecre.php
@@ -29,20 +29,22 @@
     <form method="POST" action="/ModificacionesBD/ModificaSecre.php">
                 <div class="contenedor-datos" style="float: center;">
                 <input id="numeroe" class="conteDatos" type="text" placeholder="NO. EMPLEADO" name="clave2" value="<?php echo $row['IdSec'];?>" readonly>
-                <input id="nombre" class="conteDatos" type="text" placeholder="NOMBRE" name="nombre" value="<?php echo $row['Nombre'];?>">
-                <input id="ap" class="conteDatos" type="text" placeholder="AP. PATERNO" name="apellidoP" value="<?php echo $row['ApePaterno'];?>">
+                <input id="nombre" class="conteDatos" type="text" placeholder="NOMBRE" name="nombre" value="<?php echo $row['Nombre'];?>" required minlenght="3">
+                <input id="ap" class="conteDatos" type="text" placeholder="AP. PATERNO" name="apellidoP" value="<?php echo $row['ApePaterno'];?>" required minlength="3" >
                 <input id="am" class="conteDatos" type="text" placeholder="AP. MATERNO" name="apellidoM" value="<?php echo $row['ApeMaterno'];?>">
-                <input id="calle" class="conteDatos2" type="text" placeholder="CALLE Y NÚMERO" name="calle" value="<?php echo $row['Calle'];?>">
-                <input id="colonia" class="conteDatos2" type="text" placeholder="COLONIA" name="colonia" value="<?php echo $row['Colonia'];?>">
-                <input id="municipio" class="conteDatos2" type="text" placeholder="MUNICIPIO" name="municipio" value="<?php echo $row['Municipio'];?>"> 
-                <input id="estado" class="conteDatos2" type="text" placeholder="ESTADO" name="estado" value="<?php echo $row['Estado'];?>">
-                <input id="cp" class="conteDatos3" type="text" placeholder="CÓDIGO POSTAL" name="cp" value="<?php echo $row['CP'];?>">
-                <input id="tel" class="conteDatos3" type="text" placeholder="TELÉFONO" name="tel" value="<?php echo $row['Telefono'];?>">
-                <input id="correo" class="conteDatos3" type="text" placeholder="CORREO" name="correo" value="<?php echo $row['Correo'];?>">
+                <input id="calle" class="conteDatos2" type="text" placeholder="CALLE Y NÚMERO" name="calle" value="<?php echo $row['Calle'];?>" required minlength="5" >
+                <input id="colonia" class="conteDatos2" type="text" placeholder="COLONIA" name="colonia" value="<?php echo $row['Colonia'];?>" required minlength="4" >
+                <input id="municipio" class="conteDatos2" type="text" placeholder="MUNICIPIO" name="municipio" value="<?php echo $row['Municipio'];?>" required minlength="4" > 
+                <input id="estado" class="conteDatos2" type="text" placeholder="ESTADO" name="estado" value="<?php echo $row['Estado'];?>" required minlength="4" >
+                <input id="cp" class="conteDatos3" type="text" placeholder="CÓDIGO POSTAL" name="cp" value="<?php echo $row['CP'];?>" required pattern="[\d]{5}$">
+                <input id="tel" class="conteDatos3" type="text" placeholder="TELÉFONO" name="tel" value="<?php echo $row['Telefono'];?>" required pattern="[\d]{10}$">
+                <input id="correo" class="conteDatos3" type="text" placeholder="CORREO" name="correo" value="<?php echo $row['Correo'];?>" required pattern="([a-zA-Z0-9áéíóúÁÉÍÓÚñÑ])+@+([a-zA-Z0-9])+.+([a-zA-Z0-9. ])">
                 </div>
                 <div class="contenedor-botones" style="float: center;">
-                <input disabled id="btn" class="botones" type="submit" name="modifica" value="EDITAR" onclick="location.href='/ModificacionesBD/ModificaSecre.php'">
-                <input disabled id="btn2" class="botones" type="submit" name="elimina" value="ELIMINAR" onclick="location.href='/ModificacionesBD/ModificaSecre.php'">
+                <!-- <input  id="btn" class="botones" type="submit" name="modifica" value="EDITAR" onclick="location.href='/ModificacionesBD/ModificaSecre.php'">
+                <input  id="btn2" class="botones" type="submit" name="elimina" value="ELIMINAR" onclick="location.href='/ModificacionesBD/ModificaSecre.php'"> -->
+                <input  id="btn" class="botones" type="submit" name="modifica" value="EDITAR" onclick="validar()">
+                <input  id="btn2" class="botones" type="submit" name="elimina" value="ELIMINAR" onclick="eliminar()">
                 </div>
                 </form>
             </div>
