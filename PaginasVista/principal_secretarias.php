c0d39cfbe938f3cb93a68db632b5973b5eaeeba5
AlmaSerrano <yarigonzalezserrano12@gmail.com>
2022-11-26 17:12:26 -0600

    Cambios en principal secretarias

--- PaginasVista/principal_secretarias.php
+++ PaginasVista/principal_secretarias.php
@@ -119,20 +119,87 @@
             </div>
             
             <div class="contenedor-alumnos">
-                <!--<div class="contenedor-imageA" style="float: center;">
-                    <img src="/img/Alumnos.png" alt="" width="92%"> 
-                    <span class="titulo">DATOS GENERALES</span>
+                <div class="contenedor-imageA" style="float: center;">
+                    <img src="/img/calendario.jpg" alt="" width="100%"> 
+                    <span class="titulo">PLANEACIÓN</span>
                 </div> 
-                <div class="botonesCA" >
-                    <button class="btn-consultarA">
+                <div class="botonesPla" >
+                    <button class="btn-fechasP">
                         <div class="icono" style="margin-top: -15px;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="5" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 5">
                                 <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
                             </svg>
                         </div>
-                        <span><b><a href="">CONSULTAR</b></span></a>
+                        <span><b><a href="">FECHAS PLANEADAS</b></span></a>
                     </button> 
-                </div>-->
+                </div>
+                <div class="botonCor" >
+                    <button class="btn-fechasC">
+                        <div class="icono" style="margin-top: -5px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="5" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                            </svg>
+                        </div>
+                        <span><b><a href=>FECHAS DE CORTE</b></span></a>
+                    </button> 
+                </div> 
+                <div class="botonFin" >
+                    <button class="btn-finalizar">
+                        <div class="icono" style="margin-top: -5px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="5" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                            </svg>
+                        </div>
+                        <span><b><a href="/PaginasVista/consulta_carreras.html">FINALIZAR SEMESTRE</b></span></a>
+                    </button> 
+                </div> 
+            </div>
+            
+            <div class="contenedor-materias">
+                <div class="contenedor-imagenMat" style="float: center;">
+                    <img src="/img/materias.jpg" alt="" width="100%"> 
+                    <span class="titulo">MATERIAS</span>
+                </div> 
+                <div class="botonesRM" >
+                    <button class="btn-registrarMat">
+                        <div class="icono">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="5" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                            </svg>
+                        </div>
+                        <span><b><a href="/PaginasVista/registro_materias.php">   REGISTRAR</b></span></a>
+                    </button> 
+                </div>  
+                <div class="botonMM" >
+                    <button class="btn-modificarMat">
+                        <div class="icono" style="margin-top: -5px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="5" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                            </svg>
+                        </div>
+                        <span><b><a href="/PaginasVista/modificar_materias.html">MODIFICAR</b></span></a>
+                    </button> 
+                </div> 
+                <div class="botonCM" >
+                    <button class="btn-consultarMat">
+                        <div class="icono" style="margin-top: -5px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="5" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                            </svg>
+                        </div>
+                        <span><b><a href="/PaginasVista/consulta_materias.html">CONSULTAR</b></span></a>
+                    </button> 
+                </div>
+                <div class="botonAs" >
+                    <button class="btn-asignarM">
+                        <div class="icono" style="margin-top: -5px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="5" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                            </svg>
+                        </div>
+                        <span><b><a href="">ASIGNAR</b></span></a>
+                    </button> 
+                </div>  
             </div>
         </div>
     </form>
