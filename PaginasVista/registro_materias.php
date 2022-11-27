fcee40ed6286c6b9a4a5984e0f7898d21f12df40
ivan7351 <carlosivanmunoz3@gmail.com>
2022-11-26 21:15:46 -0600

    materias validaciones

--- PaginasVista/registro_materias.php
+++ PaginasVista/registro_materias.php
@@ -35,11 +35,11 @@ $resultado= sqlsrv_query($conexion,$query);
             </div>
             <form  method="POST" action="../ModificacionesBD/InsertaMateria.php" class="cajas_de_texto_materias">
                 <label >Clave</label>
-                <input class="caja_texto" type="text" placeholder="(EJEMPLO: TNM1234567899)" name="clave" id="clave">
+                <input class="caja_texto" type="text" placeholder="(EJEMPLO: ASD-1234)" name="clave" id="clave" required pattern="^([a-zA-ZáéíóúÁÉÍÓÚñÑ.]{3})-([0-9]{4})$">
                 <label >Nombre</label>
-                <input class="caja_texto" type="text" placeholder="(EJEMPLO: JUAN)" name="nombre" id="nombre">
+                <input class="caja_texto" type="text" placeholder="(EJEMPLO: SISTEMAS)" name="nombre" id="nombre" required pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ. ]{3,30}$">
                 <label >Créditos</label>
-                <input class="caja_texto" type="text" placeholder="(EJEMPLO: ARTEAGA)" name="creditos" id="creditos">
+                <input class="caja_texto" type="text" placeholder="(EJEMPLO: 1)" name="creditos" id="creditos" required pattern="^[0-9]{1,2}$">
                 <div>
                 <label >Carreras</label>
                 <select class="combobox" name="carre" id="carre">
@@ -83,11 +83,13 @@ $resultado= sqlsrv_query($conexion,$query);
                 <label>Objetivos</label>      
                 <textarea name="objetivos" id="objetivos" class="textarea" cols="45" rows="8"></textarea>
 
-                    <button id="btn" class="btn_guardar" name="guardar" onclick="location.href = '../ModificacionesBD/InsertaMateria.php' ">GUARDAR</button>
+                    <!-- <button id="btn" class="btn_guardar" name="guardar" onclick="location.href = '../ModificacionesBD/InsertaMateria.php' ">GUARDAR</button> -->
+                    <button id="btn" class="btn_guardar" name="guardar" onclick="validar()">GUARDAR</button>
                     <button class="btn_cancelar" name="cancela" onclick="location.href='http://localhost/index.php'">CANCELAR</button>    
             </form>
         </div>
     </div>
     <script src="../SesionesUsuario/session_expiracion.js"></script>
+    <script src="../PaginasVista/script/cajas_materias.js"></script>
 </body>
 </html>
---
+++ PaginasVista/script/cajas_materias.js
@@ -0,0 +1,99 @@
+var b1=false;
+var b2=false;
+var b3=false;
+
+const expresiones = {
+    clave:/^[a-zA-Z]{3}\-[0-9]{4}$/,
+    nom:/^[a-zA-ZáéíóúÁÉÍÓÚñÑ. ]{3,30}$/,
+    creditos:/^[0-9]{1,2}$/
+}
+
+const clave = document.getElementById('clave');
+clave.addEventListener('keyup', (e) => {
+    let valorinput = e.target.value;
+    console.log(valorinput);
+    var i = 0;
+    var encontrado = false;
+
+    clave.value = valorinput.replace(/\s/g, '').trim();
+    console.log(valorinput);
+    if (expresiones.clave.test(valorinput.replace(/\s/g, '').trim() ) && encontrado == false) {
+        b1 = true;
+        clave.removeAttribute("style");
+        clave.style.border = "5px solid green";
+        /* validar(); */
+
+    }
+    else {
+        b1 = false;
+        clave.removeAttribute("style");
+        clave.style.border = "5px solid red";
+        /* validar(); */
+    }
+});
+
+const nombre = document.getElementById('nombre');
+nombre.addEventListener('keyup', (e) => {
+    let valorinput = e.target.value;
+    console.log(valorinput);
+    var i = 0;
+    var encontrado = false;
+
+    nombre.value = valorinput.replace(/\s/g, '').trim();
+    console.log(valorinput);
+    if (expresiones.nom.test(valorinput.replace(/\s/g, '').trim() ) && encontrado == false) {
+        b2 = true;
+        nombre.removeAttribute("style");
+        nombre.style.border = "5px solid green";
+        /* validar(); */
+
+    }
+    else {
+        b2 = false;
+        nombre.removeAttribute("style");
+        nombre.style.border = "5px solid red";
+        /* validar(); */
+    }
+});
+
+const creditos = document.getElementById('creditos');
+creditos.addEventListener('keyup', (e) => {
+    let valorinput = e.target.value;
+    console.log(valorinput);
+    var i = 0;
+    var encontrado = false;
+
+    creditos.value = valorinput.replace(/\s/g, '').trim();
+    console.log(valorinput);
+    if (expresiones.creditos.test(valorinput.replace(/\s/g, '').trim() ) && encontrado == false) {
+        b3 = true;
+        creditos.removeAttribute("style");
+        creditos.style.border = "5px solid green";
+        /* validar(); */
+
+    }
+    else {
+        b3 = false;
+        creditos.removeAttribute("style");
+        creditos.style.border = "5px solid red";
+        /* validar(); */
+    }
+});
+
+function validar(){
+    const bot = document.getElementById('btn');
+    if(b1 == true && b2 == true && b3 == true ){
+        location.href = '../ModificacionesBD/InsertaMateria.php'
+    }
+    else{
+        if (b1==false){
+            clave.style.border = "5px solid red";
+        }
+        if (b2==false){
+            nombre.style.border = "5px solid red";
+        }
+        if (b3==false){
+            creditos.style.border = "5px solid red";
+        }
+    }
+}
