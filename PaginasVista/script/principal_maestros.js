f1ff0cae721b87ea61cabd921a875b87ac158c91
llamasdelacruz <tetillamas@gmail.com>
2022-11-29 19:01:39 -0600

    recuperacion de principal maestros

--- PaginasVista/script/principal_maestros.js
+++ PaginasVista/script/principal_maestros.js
@@ -17,8 +17,49 @@ document.addEventListener("DOMContentLoaded", function(){
         item_nombre.innerHTML = "Bienvenid@ " + data.nombre;
         USER_CONTENEDOR.appendChild(item_nombre);
       
-
+        RellenarTabla(data.materias);
 
     });
 });
+
+function RellenarTabla(materias) {
+
+    //le cooloco relleno a la tabla
+    for (let i = 0; i < materias.length; i++) {
+
+        var fila = TABLA.insertRow();
+        var celda_clave = document.createElement("td");
+        var celda_nombre = document.createElement("td");
+        var celda_carrera = document.createElement("td");
+
+        
+        var clave = materias[i][0];
+        var carrera = materias[i][2];
+        var clave_carrera = materias[i][3];
+
+        var clave_materia = document.createTextNode(clave);
+        var nombre_materia = document.createTextNode(materias[i][1]);
+        var carrera_materia = document.createTextNode(carrera);
+
+        var link_clave = document.createElement("a");
+        var link_nombre = document.createElement("a");
+
+        //agregamos el texto y el nombre a los links
+        link_clave.appendChild(clave_materia);
+        link_nombre.appendChild(nombre_materia);
+        
+        link_clave.href = "../PaginasVista/captura_de_calificaciones.php?claveMa="+encodeURIComponent(clave)+"&carrera="+encodeURIComponent(carrera)+"&claveCa="+encodeURIComponent(clave_carrera); 
+        link_nombre.href = "../Paginasvista/fechas_evaluacion.php?claveMa="+encodeURIComponent(clave)+"&carrera="+encodeURIComponent(carrera)+"&claveCa="+encodeURIComponent(clave_carrera);
+
+        celda_clave.appendChild(link_clave);
+        celda_nombre.appendChild(link_nombre);
+        celda_carrera.appendChild(carrera_materia);
+        
+        fila.appendChild(celda_carrera);
+        fila.appendChild(celda_clave);
+        fila.appendChild(celda_nombre);
+
+        
+    }
+}
 
