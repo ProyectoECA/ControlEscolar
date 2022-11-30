cba215d8afbd58557a1f9790668a8ce04cff8b90
llamasdelacruz <tetillamas@gmail.com>
2022-11-29 18:57:17 -0600

    vinculacion y poner el nombre de usuario en la ventana de alumnos

--- PaginasVista/script/principal_alumnos.js
+++ PaginasVista/script/principal_alumnos.js
@@ -1,13 +1,13 @@
 
 
 const USER_CONTENEDOR = document.getElementById("nameuser");
-const TABLA = document.getElementById("tabla_maestros").getElementsByTagName('tbody')[0];
+
 
 
 //ejecuta el script despues que el HTML se parsea
 document.addEventListener("DOMContentLoaded", function(){
     //crea la tabla con js
-    fetch("../php/principal_maestros.php",
+    fetch("../php/nombre_usuario.php",
     {
         method:"POST"
     }).then(response => response.json())
@@ -16,9 +16,7 @@ document.addEventListener("DOMContentLoaded", function(){
         var item_nombre = document.createElement("p");
         item_nombre.innerHTML = "Bienvenid@ " + data.nombre;
         USER_CONTENEDOR.appendChild(item_nombre);
-      
-
+    
 
     });
 });
-
