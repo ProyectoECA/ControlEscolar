c3f486da228fd267457b495d29821f1f66da8583
AlmaSerrano <yarigonzalezserrano12@gmail.com>
2022-12-05 11:45:34 -0600

    Cambios en reticula

--- PaginasVista/reticula.php
+++ PaginasVista/reticula.php
@@ -281,26 +281,8 @@ sqlsrv_close($con);
                 </td>
             </tr>
             </div>
-            <div>
-            <table border="1"  width="130.5" height="0" id="tab">
-            <tr>
-                <td width="130.5" height="100">
-                    <?php
-                    echo $r8;
-                    ?>
-                </td>
-            </tr>
-            </div>
-            <div>
-            <table border="1"  width="130.5" height="0" id="tab">
-            <tr>
-                <td width="130.5" height="100">
-                    <?php
-                    echo $r9;
-                    ?>
-                </td>
-            </tr>
-            </div>
+            
+            
     </div>
 </body>
 </html>
