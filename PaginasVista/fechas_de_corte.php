d61e0fd05c63b9ed9fe816e8c9b2aa820281b72c
Aldo6890 <aldocastillos433@gmail.com>
2022-12-06 11:16:55 -0600

    Correcion del estilo

--- PaginasVista/fechas_de_corte.php
+++ PaginasVista/fechas_de_corte.php
@@ -21,7 +21,7 @@ $resultado3=$cone->Buscar($query3,$parametros);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fechas de corte</title>
-    <link rel="stylesheet" href=".../css/estilo_fechas_corte.css">
+    <link rel="stylesheet" href="../css/estilo_fechas_corte.css">
 </head>
 <body>
     <div class="nombre" style="float: center;">
